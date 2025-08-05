6ff3b82423ac0e2459c6aae54e927badfb5678d7
seungho-jg <tmdgh971229@gmail.com>
2025-08-05 22:57:20 +0900

    add: User Entity 추가

---
+++ src/main/backend/domain/user/entity/User.java
@@ -0,0 +1,17 @@
+package domain.user.entity;
+
+public class User{
+  private  Long id;
+  private String username;
+  private String password;
+  // 생성자
+  public User(Long id, String username, String password) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+  }
+  // getter
+  public Long getId(){ return this.id;}
+  public String getUsername(){ return this.username;}
+  public String getPassword() {return this.password;}
+}
